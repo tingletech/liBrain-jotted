0aae6a9255e6074f879eab6f1919d250b19988e9
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2016-02-22 23:14:16 -0800

    js fiddle like demo of liBrian

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+/*eslint no-new: 0*/
+/*global Jotted*/
+new Jotted(document.querySelector('#editor'), {
+  files: [{
+    type: 'html',
+    url: 'example.html'
+  }],
+  pane: 'html',
+  plugins: [
+    'codemirror'
+  ]
+});
